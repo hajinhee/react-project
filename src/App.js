0d6657f0c7443bbb5bee9bb632b0526050dc757e
hajinhee <hajinheee@gmail.com>
2025-03-10 14:19:04 +0900

    폰트어썸 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import { Button } from "@mui/material";
+import { Button, Chip } from "@mui/material";
 
 function App() {
   return (
     <>
       <div className="flex flex-1 items-center justify-center flex-col">
         <div className="">Hello, Tailwind CSS!</div>
-        <Button variant="contained">Hello world</Button>
+        <Chip label={<span>Chip Failed</span>}></Chip>
       </div>
     </>
   );
